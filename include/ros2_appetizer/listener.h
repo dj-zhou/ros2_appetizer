b1797fa282ea55b1be9336e8ed89c907cdda1d5e
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-06-20 00:47:13 -0400

    listener: add service client

--- include/ros2_appetizer/listener.h
+++ include/ros2_appetizer/listener.h
@@ -2,6 +2,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
 
 using namespace rclcpp;
 
@@ -12,5 +13,9 @@ public:
 
 private:
     void topicCallback(const std_msgs::msg::String::SharedPtr msg) const;
+    void timerCallback();
+
+    TimerBase::SharedPtr timer_;
     Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    Client<std_srvs::srv::Empty>::SharedPtr client_;
 };
--- src/listener.cpp
+++ src/listener.cpp
@@ -1,14 +1,24 @@
 #include "ros2_appetizer/listener.h"
-#include "std_msgs/msg/string.hpp"
 
+using namespace std::chrono_literals;
 using namespace std::placeholders;
 
 Listener::Listener() : Node("listener") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "chatter", 10, std::bind(&Listener::topicCallback, this, _1));
+    timer_ =
+        this->create_wall_timer(5s, std::bind(&Listener::timerCallback, this));
+    client_ = this->create_client<std_srvs::srv::Empty>("toggle");
 }
 
 void Listener::topicCallback(const std_msgs::msg::String::SharedPtr msg) const {
     RCLCPP_INFO(this->get_logger(), "Received a string: '%s'",
                 msg->data.c_str());
+}
+
+void Listener::timerCallback() {
+    auto request = std::make_shared<std_srvs::srv::Empty::Request>();
+    RCLCPP_INFO(this->get_logger(), "Calls a <std_srvs::srv::Empty> service.");
+    auto result = client_->async_send_request(request);
+    ( void )result;
 }
