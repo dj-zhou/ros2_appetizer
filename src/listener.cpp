adeddec6428b751c13e56e37d499c1081bcd5820
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-10-24 14:44:52 -0400

    f**k it, cannot run

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,5 +1,6 @@
 #include "ros2_appetizer/listener.h"
 #include "agv_interfaces/srv/add_three_ints.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 using namespace std::placeholders;
@@ -37,10 +38,25 @@ void Listener::timerAddCallback() {
     request->a = 1;
     request->b = 2;
     request->c = 3;
-    auto response = client_add_->async_send_request(request);
-    RCLCPP_INFO(this->get_logger(),
-                "Gets a <agv_interfaces::srv::AddThreeInts> response.");
-    // get stuck here
-    auto result = response.get();
-    // RCLCPP_INFO(this->get_logger(), "Sum: %ld", result.get()->sum);
+    while (!client_add_->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                         "Interrupted while waiting for the service. Exiting.");
+            return;
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                    "service not available, waiting again...");
+    }
+
+    auto result = client_add_->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(),
+                                           result)
+        == rclcpp::FutureReturnCode::SUCCESS) {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld",
+                    result.get()->sum);
+    }
+    else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                     "Failed to call service add_two_ints");
+    }
 }
